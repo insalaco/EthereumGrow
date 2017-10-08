057f3287c7db4c7007ae1a734637e7233d3b8558
Michael Insalaco <insalaco01@gmail.com>
2017-10-08 13:55:07 -0400

    remove tokens from schema

--- db/schema.rb
+++ db/schema.rb
@@ -54,19 +54,6 @@ ActiveRecord::Schema.define(version: 20171008174223) do
     t.index ["name"], name: "index_exchanges_on_name", unique: true, using: :btree
   end
 
-  create_table "tokens", force: :cascade do |t|
-    t.string   "name"
-    t.string   "chart_url"
-    t.integer  "company_id"
-    t.integer  "exchange_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["company_id"], name: "index_tokens_on_company_id", using: :btree
-    t.index ["exchange_id"], name: "index_tokens_on_exchange_id", using: :btree
-  end
-
   add_foreign_key "charts", "companies"
   add_foreign_key "charts", "exchanges"
-  add_foreign_key "tokens", "companies"
-  add_foreign_key "tokens", "exchanges"
 end
