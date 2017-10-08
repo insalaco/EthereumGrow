71d29436b83139eb50c96d029c3271b44eb50b32
Michael Insalaco <insalaco01@gmail.com>
2017-10-08 13:51:31 -0400

    add associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+  has_many :charts
+  has_many :exchanges, through: :charts
 end
--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,2 +1,4 @@
 class Exchange < ApplicationRecord
+  has_many :charts
+  has_many :companies, through: :charts
 end
