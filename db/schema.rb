3da1fd0788dd6a38c9dae6a7de65135a8fd51040
Michael Insalaco <insalaco01@gmail.com>
2017-10-08 13:21:45 -0400

    create join table for companies and exchanges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008165010) do
+ActiveRecord::Schema.define(version: 20171008171640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20171008165010) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "company_exchanges", force: :cascade do |t|
+    t.integer  "company_id"
+    t.integer  "exchange_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["company_id"], name: "index_company_exchanges_on_company_id", using: :btree
+    t.index ["exchange_id"], name: "index_company_exchanges_on_exchange_id", using: :btree
+  end
+
   create_table "exchanges", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
@@ -45,4 +54,6 @@ ActiveRecord::Schema.define(version: 20171008165010) do
     t.index ["name"], name: "index_exchanges_on_name", unique: true, using: :btree
   end
 
+  add_foreign_key "company_exchanges", "companies"
+  add_foreign_key "company_exchanges", "exchanges"
 end
---
+++ test/models/company_exchange_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CompanyExchangeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
