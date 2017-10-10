41a25bb914b1b4a04fc3b5f768ca07e6000609dd
Michael Insalaco <insalaco01@gmail.com>
2017-10-10 06:28:23 -0400

    create pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @companies = Company.select("companies.*, COUNT(DISTINCT exchanges.id) exchange_count").joins(:exchanges).group("companies.id")
   end
 end
