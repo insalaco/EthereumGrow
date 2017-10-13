08ec31eed0d71a41d6532b67f2417c9a1b229ad3
Michael Insalaco <insalaco01@gmail.com>
2017-10-13 16:37:18 -0400

    add published true constraint to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index    
-    @companies = Company.with_comments_count.order(ico_open_date: "desc").limit(3)
+    @companies = Company.with_comments_count.order(ico_open_date: "desc").where(published: true).limit(3)
   end
 end
