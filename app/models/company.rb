8670c09da5d9b1753fd29b9cad10433b46f310bd
Michael Insalaco <insalaco01@gmail.com>
2017-11-09 14:57:25 -0500

    add dependent destroy to models

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,8 +4,9 @@ class Company < ApplicationRecord
   
   has_many :company_exchanges
   has_many :exchanges, through: :company_exchanges
-  has_many :currencies
-  has_one :token
+  has_many :currencies, dependent: :destroy
+  has_one :token, dependent: :destroy
+  accepts_nested_attributes_for :token
     
   scope :with_comments_count, -> { left_outer_joins(:exchanges).select("companies.*, COUNT(exchanges.id) exchange_count").group("companies.id") }
 end
