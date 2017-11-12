01615a1dfe2249e71e37a973aec6c68cc47a40bf
Michael Insalaco <insalaco01@gmail.com>
2017-11-11 20:31:26 -0500

    make migration reversible

--- db/migrate/20171112011949_remove_company_from_currencies.rb
+++ db/migrate/20171112011949_remove_company_from_currencies.rb
@@ -1,5 +1,5 @@
 class RemoveCompanyFromCurrencies < ActiveRecord::Migration[5.0]
   def change
-    remove_column :currencies, :company_id
+    remove_column :currencies, :company_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112011949) do
+ActiveRecord::Schema.define(version: 20171112011212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20171112011949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "token_id"
+    t.integer  "company_id"
     t.index ["token_id"], name: "index_currencies_on_token_id", using: :btree
   end
 
