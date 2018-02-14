e39cb9c86a82110911cf6276f90b0aadfec26d0c
Michael Insalaco <insalaco01@gmail.com>
2018-02-14 13:10:32 -0500

    change title "Etherem Grow"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "EthereumGrow"
+    base_title = "Ethereum Grow"
     if page_title.empty?
       base_title
     else
