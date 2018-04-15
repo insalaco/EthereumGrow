a76874b6e7980cae96f35496a0f1a304d38d748f
Michael Insalaco <insalaco01@gmail.com>
2018-04-15 07:40:10 -0400

    change order to most recent

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index    
-    @companies = Company.includes(:token).where(published: true, dapp: true, alliance: false).order(ico_close_date: "DESC", name: "ASC").limit(3)
+    @companies = Company.includes(:token).where(published: true, dapp: true, alliance: false).order("created_at DESC").limit(3)
   end
 end
